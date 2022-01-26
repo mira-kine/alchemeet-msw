d981bc601be74068be4f159f67ea58cfe82e71b2
mira-kine <mirak0419@gmail.com>
2022-01-26 15:41:05 -0800

    passing one part of msw test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,14 +4,25 @@
 import { screen, render } from '@testing-library/react'
 // 🚨
 // import rest
+import { rest } from 'msw'
 // import setupServer
+import { setupServer } from 'msw/node'
 import App from './App'
+
+// use this API URL: https://uzgiamkrbapxufnwdrja.supabase.co/rest/v1/users
+/**
+ * Goes inside the callback function body
+ *     const select = req.url.searchParams.get('select')
+    if (select === '*') {
+      return res()
+    }
+ */
 
 const user = {
   id: 1,
   created_at: '2021-12-13T00:17:29+00:00',
   // 🚨 Add a name here
-  name: '',
+  name: 'Vonta',
   avatar: 'https://thumbs.gfycat.com/NiceRequiredGrunion-size_restricted.gif',
   header: 'https://static.wikia.nocookie.net/naruto/images/5/50/Team_Kakashi.png',
   likes: ['React', 'Anime', 'Traveling', 'Living', 'Tower Defense Games', 'Card Games'],
@@ -20,12 +31,20 @@ const user = {
 }
 
 // 🚨 Create your server
+const server = setupServer(
+  rest.get(`https://uzgiamkrbapxufnwdrja.supabase.co/rest/v1/users`, (req, res, ctx) => {
+    const select = req.url.searchParams.get('select')
+    if (select === '*') {
+      return res(ctx.json([user]))
+    }
+  })
+)
 
 // 🚨 Listen for server start
-beforeAll()
+beforeAll(() => server.listen())
 
 // 🚨 Close server when complete
-afterAll()
+afterAll(() => server.close())
 
 test('Should render the header', async () => {
   render(<App />)
